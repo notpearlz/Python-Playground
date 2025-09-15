dd8d6d7b8d36cf2ab6c60d8d3a196679f04626c2
Duy <lolmebunbun@gmail.com>
2025-09-15 12:52:20 -0500

    Finish simple calculator

--- Calculator.py
+++ Calculator.py
@@ -1,41 +1,76 @@
 def main():
 
-    start()
+    calculator()
 
 
 
 
-def start():
+def calculator():
     print("Welcome to my calculator app")
 
-    print("Please enter a math operation")
+    print("Please enter a simple math operation")
+    print("Type 'x' to exit the app")
 
-    operation = input()
-    if(isValidOperation(operation)):
-        print("Operation is valid")
+    while True:
+        print('----------------------------------- \n\n\n')
+        operation = input()
+
+        if(operation == 'x'):
+            break
+
+
+        if(isValidOperation(operation)):
+            operationList = operation.split(' ')
+
+            num1 = int(operationList[0])
+            operator = operationList[1]
+            num2 = int(operationList[2])
+
+
+            result = 0
+
+            if(operator == '+'):
+                result = add(num1,num2);
+            elif(operator == '-'):
+                result = subtract(num1,num2);
+            elif(operator == '*'):
+                result = multiply(num1,num2);
+            elif(operator == '/'):
+                result = divide(num1,num2);
+
+            print(result);
+        else:
+            print("NOT A VALID EQUATION")
+
 
     
+
 
 def isValidOperation(operation):
     operationList = operation.split(' ')
 
-    for x in operationList:
-        print(x)
+    if(len(operationList) == 3): 
+       
+
+        return True
+
+    else:
+        return False
+
         
-    return True
 
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def subtract(num1, num2):
-    pass
+    return num1 - num2
 
 def divide(num1, num2):
-    pass
+    return num1 / num2
 
 def multiply(num1, num2):
-    pass
+    return num1 * num2
 
 
 
